4bae8267e98a53d8d868fea65bdd795f51264c2c
eschmidt <eric.purdue@gmail.com>
2012-05-24 21:29:39 -0600

    -

--- model/src/main/java/com/example/entity/DateObject.java
+++ model/src/main/java/com/example/entity/DateObject.java
@@ -6,10 +6,20 @@ import java.util.Date;
 public class DateObject implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private long id;
+
     private Date date;
 
     private Date lastAccessed;
 
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public Date getDate() {
         return this.date;
